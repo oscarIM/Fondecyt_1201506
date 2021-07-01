f7146d8127ecf9ebd1788bff534c3f820e082309
oscarIM <oskinostroza@gmail.com>
2021-07-01 00:03:09 -0400

    working script

--- script_hyperCalc.R
+++ script_hyperCalc.R
@@ -3,8 +3,9 @@ library(dplyr)
 source("hyperCalc.R")
 ##
 data <- fread("All_Mammals_productivity.csv")
-#variables que pasaron el VIF
+#variables que pasaron el VIF. Solo como ejemplo
 var_names <- c("PETDriestQuarter", "PETseasonality", "PETWettestQuarter")
 #ver el numero de combinaciones pareadas
 get_comb(var_names = var_names, n_comb = 2)
-hyperV_prod <- calc_hVol(data = data,cores = 3, var_names = var_names, n_occs =100, n_comb = 2,samples_per_points = 10)
+##cada core use como 600 Mb de RAM
+hyperV_prod <- calc_hVol(data = data, cores = 20, var_names = var_names, n_occs = 100, n_comb = 2, samples_per_points = 10)
